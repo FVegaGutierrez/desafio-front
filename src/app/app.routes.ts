9d0989997ec73fabb9eb3771acc67c6593b91bc4
PipeLaPipa <fel.vegag@duocuc.cl>
2024-08-15 13:50:49 -0400

    inicio-desafio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent }, // Ruta principal
+  { path: '', component: InicioComponent }, //Cuando la ruta viene vacia
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }  
